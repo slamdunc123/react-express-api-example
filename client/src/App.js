a342995e533db4b5030af7639c386e0c630047a6
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-11-05 09:59:21 +0000

    bootstrap removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // router
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -19,20 +16,6 @@ function App() {
 				<Header />
 				<Main />
 				<Footer />
-				{/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
 			</div>
 		</Router>
 	);
